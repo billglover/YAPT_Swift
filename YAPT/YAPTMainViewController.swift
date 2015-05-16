864223b0df79428d3193cdf0ef79f32e141d10b4
Bill Glover <bill@billglover.co.uk>
2015-05-16 17:47:45 +0100

    Basic controller complete

---
+++ YAPT/YAPTMainViewController.swift
@@ -0,0 +1,161 @@
+//
+//  YAPTMainViewController.swift
+//  YAPT
+//
+//  Created by Bill Glover on 09/05/2015.
+//  Copyright (c) 2015 Bill Glover. All rights reserved.
+//
+
+import UIKit
+
+class YAPTMainViewController: UIViewController {
+
+    // MARK: - Types
+    private enum IntervalType { case Work, Break }
+    private typealias Interval = (type:IntervalType, duration:Double)
+    
+    // MARK: - UI Properties
+    @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var timerButton: UIButton!
+    @IBOutlet var timerView: UIView!
+    @IBInspectable var breakColor = UIColor(red: 48/255.0, green: 119/255.0, blue: 198/255.0, alpha: 1.0)
+    @IBInspectable var workColor = UIColor(red: 194/255, green: 49/255.0, blue: 52/255.0, alpha: 1.0)
+    
+    // MARK: - Properties
+    private var timer = NSTimer()
+    private var schedule = [Interval]()
+    private let timerInterval: NSTimeInterval = 1.0
+    private var currentIntervalStartTime = NSDate()
+    private var remainingIntervalDuration: NSTimeInterval = 0.0 {
+        didSet {
+
+            // convert duration into minutes:seconds
+            let minutes: Double = floor(round(remainingIntervalDuration) / 60)
+            let seconds: Double = trunc(round(remainingIntervalDuration) - minutes * 60)
+            
+            // display as mm:ss
+            let labelString = String(format: "%02.0f:%02.0f", (minutes), (seconds))
+            timerLabel.text = labelString
+        }
+    }
+    private var currentIntervalIndex = 0 {
+        didSet {
+            
+            // as we've changed the interval we need to invalidate the timer and update remaining inverval
+            timer.invalidate()
+            remainingIntervalDuration = schedule[currentIntervalIndex].duration
+        }
+    }
+
+    // MARK: - Timer Methods
+    private func startTimer() {
+        println("startTimer() called")
+        
+        // set the remaining duration
+        remainingIntervalDuration = schedule[currentIntervalIndex].duration
+        
+        // capture the start time
+        currentIntervalStartTime = NSDate()
+        println("intervalIndex \(currentIntervalIndex) started at \(currentIntervalStartTime)")
+        
+        // set up the timer
+        timer = NSTimer.scheduledTimerWithTimeInterval(timerInterval, target: self, selector: Selector("timerFired"), userInfo: nil, repeats: true)
+    }
+
+    private func interruptTimer() {
+        println("interruptTimer() called")
+        
+        // stop the timer
+        timer.invalidate()
+    }
+    
+    private func completeTimer() {
+        println("completeTimer() called")
+        
+        // stop the timer
+        timer.invalidate()
+        
+        // print some diagnostic stats
+        println("target duration: \(schedule[currentIntervalIndex].duration)")
+        println("actual duration: \(currentIntervalStartTime.timeIntervalSinceNow * -1)")
+        
+        // set the remaining duration to be zero
+        remainingIntervalDuration = 0.0
+        
+        // advance the currentIntervalIndex or loop back to the start
+        currentIntervalIndex = (currentIntervalIndex + 1) % schedule.count
+        
+        // toggle the timer button so that we can start again
+        updateDisplay()
+    }
+    
+    private func resetTimer() {
+        println("resetTimer() called")
+    }
+    
+    func timerFired() {
+        println("timerFired() called at \(NSDate())")
+        
+        // calculate the remaining duration for this interval
+        let currentTimerDuration: NSTimeInterval = currentIntervalStartTime.timeIntervalSinceNow
+        remainingIntervalDuration = schedule[currentIntervalIndex].duration + currentTimerDuration
+        
+        // if we are out of time, complete the timer
+        if round(remainingIntervalDuration) <= 0.0 {
+            completeTimer()
+        }
+    }
+    
+    // MARK: - Update Display
+    private func updateDisplay() {
+        updateBackground()
+        updateTimerButton()
+    }
+    
+    private func updateTimerButton() {
+        if timer.valid {
+            timerButton.setTitle("Stop", forState: UIControlState.Normal)
+        } else {
+            timerButton.setTitle("Start", forState: UIControlState.Normal)
+        }
+    }
+    
+    private func updateBackground() {
+        switch schedule[currentIntervalIndex].type {
+        case .Work:
+            timerView.backgroundColor = workColor
+        case .Break:
+            timerView.backgroundColor = breakColor
+        }
+    }
+    
+    // MARK: - Navigation
+    @IBAction func timerButtonPressed(sender: AnyObject) {
+        if let timerButtonText = timerButton.titleLabel?.text {
+            switch timerButtonText {
+            case "Start":
+                startTimer()
+            case "Stop":
+                interruptTimer()
+            default:
+                break
+            }
+        }
+        updateTimerButton()
+    }
+    
+    // MARK: - View Controller Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // load a test schedule
+        schedule.append((type: IntervalType.Work, duration:10.0))
+        schedule.append((type: IntervalType.Break, duration:5.0))
+        schedule.append((type: IntervalType.Work, duration:10.0))
+        schedule.append((type: IntervalType.Break, duration:15.0))
+        currentIntervalIndex = 0
+        
+        updateDisplay()
+    }
+
+}
