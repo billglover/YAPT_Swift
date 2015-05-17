b6145ffeec71dc2b306a39244e62658bda9702e1
Bill Glover <bill@billglover.co.uk>
2015-05-17 06:54:54 +0100

    Register notification settings

--- YAPT/AppDelegate.swift
+++ YAPT/AppDelegate.swift
@@ -16,6 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        registerNotificationTypes()
+        
         return true
     }
 
@@ -41,6 +43,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
+    func registerNotificationTypes() {
+        let types = UIUserNotificationType.Alert | UIUserNotificationType.Sound
+        let settings = UIUserNotificationSettings(forTypes: types, categories: nil)
+        UIApplication.sharedApplication().registerUserNotificationSettings(settings)
+    }
 
 }
 
