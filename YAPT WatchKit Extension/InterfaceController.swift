cebde8d17ef2292639ced3ca4de789f5ae785f9c
Bill Glover <bill@billglover.co.uk>
2015-05-25 15:15:25 +0100

    Added WatchKit Target

---
+++ YAPT WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  YAPT WatchKit Extension
+//
+//  Created by Bill Glover on 25/05/2015.
+//  Copyright (c) 2015 Bill Glover. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
